5b5890a8638244944b103c1bed8e4c9f36eeb6c3
Dominik Dabkowski <dominik.dabkowski@ulb.be>
2022-04-11 23:26:08 +0200

    the admin token table is now created and the admin token is added

--- DatabaseCreator/src/main/java/tablecreators/AdminTokenHandler.java
+++ DatabaseCreator/src/main/java/tablecreators/AdminTokenHandler.java
@@ -1,7 +1,52 @@
 package tablecreators;
 
 
-import java.sql.*;
+import java.security.NoSuchAlgorithmException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-public class AdminTokenHandler {
+/**Method responsible for creating the admin_token table
+ * and adding the admintoken
+ */
+public class AdminTokenHandler extends ConnectionHandler {
+    private final String tokenColumn = "token";
+    public AdminTokenHandler() throws NoSuchAlgorithmException, SQLException {
+        createTable();
+        addAdminToken();
+        showTable();
+    }
+
+    /**Creates the admin_token table
+     *
+     */
+    @Override
+    public void createTable() {
+        String createQuery = """
+                CREATE TABLE IF NOT EXISTS admin_token (
+                id int UNSIGNED AUTO_INCREMENT,
+                token VARCHAR(64) NOT NULL UNIQUE,
+                PRIMARY KEY (id));""";
+        realizeUpdateQuery(createQuery);
+    }
+
+    /**Adds the admin token
+     *
+     */
+    public void addAdminToken() throws NoSuchAlgorithmException {
+        String adminToken = encrypt("1011str1012t1013mintok14n");
+        String insertQuery = String.format("""
+                INSERT INTO admin_token (%s) VALUES ('%s')""", tokenColumn, adminToken);
+        realizeUpdateQuery(insertQuery);
+    }
+
+    /**Show the admin token in the database
+     * @throws SQLException database exception
+     */
+    public void showTable() throws SQLException {
+        String selectQuery = "SELECT * FROM admin_token";
+        ResultSet queryResult = executeSelectQuery(selectQuery);
+        if (queryResult.next()) {
+            System.out.println(queryResult.getString(tokenColumn));
+        }
+    }
 }
