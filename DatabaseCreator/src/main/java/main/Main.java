0c72ec3c01718eec1f366aaa5363fba5d45c0947
Dominik Dabkowski <dominik.dabkowski@ulb.be>
2022-04-11 23:26:18 +0200

    filled the main class

--- DatabaseCreator/src/main/java/main/Main.java
+++ DatabaseCreator/src/main/java/main/Main.java
@@ -1,4 +1,12 @@
 package main;
 
+import tablecreators.AdminTokenHandler;
+
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+
 public class Main {
+    public static void main(String[] args) throws NoSuchAlgorithmException, SQLException {
+        new AdminTokenHandler();
+    }
 }
