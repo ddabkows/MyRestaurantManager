7a2090f885aa066f1a4ccad858cb88c0439cdefa
Dominik Dabkowski <dominik.dabkowski@ulb.be>
2022-04-11 15:21:20 +0200

    Added TablesManager module

---
+++ TablesManager/src/main/java/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+/**Simple Main file
+ * Runs the TablesManager
+ */
+public class Main {
+    // Methods
+
+    /**Main method
+     * Launches the whole program
+     */
+    public static void main() {
+        System.out.println("Main has been launched");
+    }
+}
