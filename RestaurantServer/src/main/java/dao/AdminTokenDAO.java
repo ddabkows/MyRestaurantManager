f763f23e930e3bcc2db8f55886f18106aaeb4c7d
Dominik Dabkowski <dominik.dabkowski@ulb.be>
2022-04-13 01:42:27 +0200

    Added encrypter to AdminTokenDAO

--- RestaurantServer/src/main/java/dao/AdminTokenDAO.java
+++ RestaurantServer/src/main/java/dao/AdminTokenDAO.java
@@ -1,18 +1,20 @@
 package dao;
 
+import java.security.NoSuchAlgorithmException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Objects;
 
 public class AdminTokenDAO extends DAO {
     private static final String TOKENCOLUMN = "token";
+    private final Encrypter encrypter = new Encrypter();
 
     public boolean checkAdminToken(String token) {
         String query = "SELECT * FROM admin_token";
         try (ResultSet queryResult = realizeExecuteQuery(query)) {
             queryResult.next();
-            return Objects.equals(queryResult.getString(TOKENCOLUMN), token);
-        } catch (SQLException | NullPointerException ignored) {
+            return Objects.equals(queryResult.getString(TOKENCOLUMN), encrypter.encrypt(token));
+        } catch (SQLException | NullPointerException | NoSuchAlgorithmException ignored) {
             return false;
         }
     }
