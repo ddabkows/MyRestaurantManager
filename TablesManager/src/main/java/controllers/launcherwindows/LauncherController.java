ad1fd378c1a4160c95f53ca9c1d29416f7b83305
Dominik Dabkowski <dominik.dabkowski@ulb.be>
2022-04-13 01:42:49 +0200

    Now passwordField available to check token + enter push

--- TablesManager/src/main/java/controllers/launcherwindows/LauncherController.java
+++ TablesManager/src/main/java/controllers/launcherwindows/LauncherController.java
@@ -5,16 +5,25 @@ import java.io.IOException;
 
 import client.TokenSender;
 import controllers.Controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 
 public class LauncherController extends Controller {
 
+    @FXML
+    public TextField tokenTextField;
+
     public void getToRestaurant() throws IOException {
         TokenSender tokenSender = new TokenSender(super.getClientSocket().getSocket());
-        if (tokenSender.sendPacket("123")) {
+        if (tokenSender.sendPacket(tokenTextField.getText())) {
             System.out.println("Authorized");
         } else {
             System.out.println("Rejected");
         }
+    }
+
+    public void enter() throws IOException {
+        getToRestaurant();
     }
 }
