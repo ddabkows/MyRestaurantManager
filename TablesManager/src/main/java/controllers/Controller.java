4da9eeb5e16f81b8babe332d1384b161357c16a4
Dominik Dabkowski <dominik.dabkowski@ulb.be>
2022-04-13 02:25:36 +0200

    Added staging methods

--- TablesManager/src/main/java/controllers/Controller.java
+++ TablesManager/src/main/java/controllers/Controller.java
@@ -1,13 +1,40 @@
 package controllers;
 
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
 import client.ClientSocketBuilder;
+import resources.Resources;
+
+
 
 
 public class Controller {
+    private Parent root;
     private ClientSocketBuilder clientSocket;
 
     public void setClientSocket(ClientSocketBuilder clientSocketToSet) {this.clientSocket = clientSocketToSet;}
 
     public ClientSocketBuilder getClientSocket() {return this.clientSocket;}
+
+    protected FXMLLoader setRoot(Resources resource) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource(resource.toString()));
+        root = loader.load();
+        return loader;
+    }
+
+    protected Stage getStage(ActionEvent actionEvent) {
+        return (Stage)((Node) actionEvent.getSource()).getScene().getWindow();}
+
+    protected void setStage(ActionEvent actionEvent) {
+        Stage stage = getStage(actionEvent);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+    }
 }
