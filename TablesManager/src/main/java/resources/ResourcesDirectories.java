a544e20432830fdb6594a3d0e81a56bcdea6012c
Dominik Dabkowski <dominik.dabkowski@ulb.be>
2022-04-13 02:26:04 +0200

    Main menu dir

--- TablesManager/src/main/java/resources/ResourcesDirectories.java
+++ TablesManager/src/main/java/resources/ResourcesDirectories.java
@@ -7,4 +7,5 @@ public final class ResourcesDirectories {
     private ResourcesDirectories() {}
     // Directory to the launcher window
     public static final String LAUNCHERDIR = "LauncherWindows/";
+    public static final String MAINMENUDIR = "RestaurantView/";
 }
