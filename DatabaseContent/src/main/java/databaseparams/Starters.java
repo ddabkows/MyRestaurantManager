7088ca01aa9dd12312beada70785c43aa317801c
Dominik Dabkowski <dominik.dabkowski@ulb.be>
2022-04-14 22:48:07 +0200

    Starters now have description and price

--- DatabaseContent/src/main/java/databaseparams/Starters.java
+++ DatabaseContent/src/main/java/databaseparams/Starters.java
@@ -1,7 +1,17 @@
 package databaseparams;
 
 public enum Starters {
-    EDAMAME {public final String toString() {return "Edamame";}},
-    GYOZA {public final String toString() {return "Gyoza";}},
-    KARAAGE {public final String toString() {return "Karaage";}},
+    EDAMAME("Boiled soybeans", 5.f) {public final String toString() {return "Edamame";}},
+    GYOZA("Japanese dumplings with chicken, garlic and onions", 7.f) {public final String toString() {return "Gyoza";}},
+    KARAAGE("Japanese style nuggets", 5.f) {public final String toString() {return "Karaage";}};
+
+    private final String description;
+    private final float price;
+
+    Starters(String descriptionToSet, float priceToSet) {
+        this.description = descriptionToSet;
+        this.price = priceToSet;
+    }
+
+    public String getDescription() {return this.description;}
 }
