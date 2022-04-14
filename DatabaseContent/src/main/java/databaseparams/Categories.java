7c5890dd84472dec79d3cabd31934ca3d8de0b01
Dominik Dabkowski <dominik.dabkowski@ulb.be>
2022-04-14 22:46:51 +0200

    Added taxation rate to categories

--- DatabaseContent/src/main/java/databaseparams/Categories.java
+++ DatabaseContent/src/main/java/databaseparams/Categories.java
@@ -1,8 +1,14 @@
 package databaseparams;
 
 public enum Categories {
-    STARTERS {public final String toString() {return "Starters";}},
-    CALIFORNIAS {public final String toString() {return "Californias";}},
-    MAKIS {public final String toString() {return "Makis";}},
-    CHIRASHIS {public final String toString() {return "Chirashis";}}
+    STARTERS(12) {public final String toString() {return "Starters";}},
+    URUMAKIS(12) {public final String toString() {return "Urumakis";}},
+    MAKIS(12) {public final String toString() {return "Makis";}},
+    NIGIRIS(12) {public final String toString() {return "Nigiris";}};
+
+    private final int tax;
+
+    Categories(int taxToSet) {this.tax = taxToSet;}
+
+    public int getTax() {return this.tax;}
 }
